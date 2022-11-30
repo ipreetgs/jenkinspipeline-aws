daad0ddeb2f5926ed111113053f61a4ab9e90429
gsingh <ipreetgs.gmail.com>
2022-11-30 12:30:55 +0530

    modify py file

--- s3AutomationwithJenkins.py
+++ s3AutomationwithJenkins.py
@@ -1,8 +1,10 @@
 import boto3
 import sys
+from botocore import ClientError
 
 BucketName = sys.argv[1]
 AWS_REGION = "us-east-1"
+
 session = boto3.session.Session()
 s3_client = session.client('s3')
 
@@ -11,5 +13,22 @@ s3_client = session.client('s3')
 client = boto3.client('s3')
 
 response = client.create_bucket(Bucket=BucketName)
+
+#<<<<<<<<vers>>>>>>
+resource_for_s3 = boto3.resource("s3", region_name=AWS_REGION)
+
+def VerSonEnable(bucket_name):
+    versioning = resource_for_s3.BucketVersioning(bucket_name)
+    versioning.enable()
+
+    print(f'Wow, Your versioning is enabled for S3 Bucket: {versioning.status}')
+
+
+#<<<lifecycle>>
+s3=boto3.resource('s3')
+Bucket_Lifecycle=s3.BucketLifecycle(BucketName)
+
+
+VerSonEnable(BucketName)
 
 print("Amazon S3 bucket has been created")
