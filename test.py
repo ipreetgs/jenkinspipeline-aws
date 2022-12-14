4be59f9e758ff05e9ea79cddc3ccba83bcaee906
gsingh <ipreetgs.gmail.com>
2022-12-14 14:37:32 +0530

    modify py file

--- test.py
+++ test.py
@@ -1,28 +1,53 @@
 import boto3
+from datetime import datetime
 def LifeCycle_Mgmt(BucketName,TransDay,ExpDay,NonCurrVeTrans):
     s3=boto3.resource('s3')
     bucket_lifecycle_configuration=s3.BucketLifecycleConfiguration(BucketName)
-    response=bucket_lifecycle_configuration.put(
+    response = bucket_lifecycle_configuration.put(
+        ChecksumAlgorithm='CRC32',
         LifecycleConfiguration={
             'Rules': [
                 {
                     'Expiration': {
+                        'Date': datetime(2015, 1, 1),
                         'Days': ExpDay,
                         'ExpiredObjectDeleteMarker': True
                     },
-                    'ID': 'demorule',
+                    'ID': 'demotxchdrule',
+                    'Prefix': 'string',
+                    'Filter': {
+                        'Prefix': 'string',
+                        'Tag': {
+                            'Key': 'string',
+                            'Value': 'string'
+                        },
+                        'ObjectSizeGreaterThan': 123,
+                        'ObjectSizeLessThan': 123,
+                        'And': {
+                            'Prefix': 'string',
+                            'Tags': [
+                                {
+                                    'Key': 'string',
+                                    'Value': 'string'
+                                },
+                            ],
+                            'ObjectSizeGreaterThan': 123,
+                            'ObjectSizeLessThan': 123
+                        }
+                    },
                     'Status': 'Enabled',
                     'Transitions': [
                         {
+                            'Date': datetime(2015, 1, 1),
                             'Days': TransDay,
-                            'StorageClass': 'STANDARD_IA',
+                            'StorageClass': 'STANDARD_IA'
                         },
                     ],
                     'NoncurrentVersionTransitions': [
                         {
                             'NoncurrentDays': NonCurrVeTrans,
                             'StorageClass': 'GLACIER',
-                            'NewerNoncurrentVersions': 2
+                            'NewerNoncurrentVersions': 123
                         },
                     ],
                     'NoncurrentVersionExpiration': {
@@ -34,6 +59,6 @@ def LifeCycle_Mgmt(BucketName,TransDay,ExpDay,NonCurrVeTrans):
                     }
                 },
             ]
-        }
+        },
     )
 LifeCycle_Mgmt("demotxchd1",15,20,30)
