f68338625283c1ca8ee476434335425b11ca48a2
gsingh <ipreetgs.gmail.com>
2022-12-14 12:44:15 +0530

    modify py file

--- pythonpipe.groovy
+++ pythonpipe.groovy
@@ -5,9 +5,9 @@ pipeline{
     }
    parameters {
         string description: 'Enter the name of S3 bucket', name: 'Bucket_Name'
-        string description: 'Enter the EXp date', expay: 'ExpDay'
-        string description: 'Enter the transfer date', transday: 'TransDay'
-        string description: 'Enter the name of S3 bucket', oth: 'oth'
+        string description: 'Enter the EXp date', expday: 'Exp_Day'
+        string description: 'Enter the transfer date', transday: 'Trans_Day'
+        string description: 'Enter the name of S3 bucket', oth: 'Oth_day'
     }
     stages {
         stage('Git checkout') {
